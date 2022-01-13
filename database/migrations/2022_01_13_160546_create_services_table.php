9a7c4703087182697c270e0d3c8829b3f163d919
romanKuran <roman.k@lynksen.com>
2022-01-13 18:24:12 +0200

    create database

---
+++ database/migrations/2022_01_13_160546_create_services_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('source', 250)->unique();
+            $table->string('placeholder', 250);
+            $table->text('validation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
